01cb1e6538302e407f7ee4884df70371892aee77
liurunlin <liurunlin@heweisoft.com>
2021-03-06 19:16:25 +0800

    更新demo

--- cloud2021/spring-boot-demo/src/main/java/com/study/springboot/serivce/PaymentService.java
+++ cloud2021/spring-boot-demo/src/main/java/com/study/springboot/serivce/PaymentService.java
@@ -8,5 +8,5 @@ public interface PaymentService
 {
     public int create(Payment payment);
 
-    public Payment getPaymentById(@Param("id") Integer id);
+    public Payment getPaymentById(@Param("id") Long id);
 }
---
+++ cloud2021/test/src/main/java/juc/study/TestDemo.java
@@ -0,0 +1,21 @@
+package juc.study;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        String str1 = new StringBuffer("ja").append("va").toString();
+        System.out.println(str1);
+        System.out.println(str1.intern());
+        System.out.println(str1 == str1.intern());
+        System.out.println(str1.equals(str1.intern()));
+
+        String str2 = new StringBuffer("58").append("city").toString();
+        System.out.println(str2);
+        System.out.println(str2.intern());
+        System.out.println(str2 == str2.intern());
+
+        ConcurrentHashMap<Object, Object> s = new ConcurrentHashMap<>();
+
+    }
+}
